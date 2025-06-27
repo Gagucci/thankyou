ffdd72d5d54f2830a5cfe6ad3e0642979b3e67d1
Kevin Gagante <KevinGagante@Gmail.com>
2025-06-27 14:59:53 -0700

    added to animation

--- src/main/java/com/pluralsight/art.java
+++ src/main/java/com/pluralsight/art.java
@@ -126,6 +126,23 @@ public class art {
                 "\u001B[0m"; // Reset color
         System.out.println(message);
     }
+    public static void printPersonalMessage2() {
+        String message = "\u001B[36m" + """
+                DONT FORGET ABOUT US THANK YOUUUUUU, if I become a millionare in the future it was thanks to you! - Kevin Gagante
+                Thanks for the awesome classes and fun workshops! - Abraham Ghani
+                Thank you for the awesome class and stay cool ! - J Torres
+                System.out.println("THANK YOU FOR BEING THE BEST INSTRUCTOR!!! 🐐🐐🐐 -Jacob")
+                Thank you for everything, Remgoku.   -Marq Alejandro
+                🎉🎉Thank you for sharing your wisdom with us. Your the best🎉🎉- Cricelia
+                Thanks for all your support, Remsey!! We truly appreciate you and will miss you! But see you on LinkedIn hehe 😆🥺 - Claudia!
+                It was truly an honor learning from you, Remsey. I have great respect and appreciation for the work you did to make class engaging and educational. - Jonathan Yun
+                Big thanks to Remsey for making Java way less scary, way more fun and understanable i will be taking these skill with me throughout my journey. - Freddie Finesse
+                Thank you for all your support through out the academy, you truly are a great instructor and very much appreciated keep being great 💪🏾🐐‼️ -Jakari\s
+                It has been a pleasure and a privilege to learn from you Remsey. You have my utmost respect and gratitude. -Jakeyl
+                """ +
+                "\u001B[0m"; // Reset color
+        System.out.println(message);
+    }
 
 
 }
