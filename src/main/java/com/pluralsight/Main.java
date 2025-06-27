b2021cec44f476c37ffc3f7396725d2ff51e67bb
Kevin Gagante <KevinGagante@Gmail.com>
2025-06-27 13:56:42 -0700

    init commit

---
+++ src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,10 @@
+package com.pluralsight;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        art.printArt2();
+        art.printArt();
+        art.printArt3();
+        art.printArt4();
+    }
+}
